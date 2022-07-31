dac0090fcd6743d69277bdf80f589db036f90297
Liu Kun <liukunup@163.com>
2022-07-31 23:27:30 +0800

    初始化后端模板

---
+++ src/main/java/com/liukunup/taie/TaieApplication.java
@@ -0,0 +1,13 @@
+package com.liukunup.taie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liukunup/taie/TaieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liukunup.taie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
